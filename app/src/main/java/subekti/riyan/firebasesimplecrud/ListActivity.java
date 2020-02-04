4c43859003de268365fce1f6e98929e3cccc4694
riyansubekti <riyansubekti1406@gmail.com>
2020-02-04 18:56:51 +0700

    Update and Delete

--- app/src/main/java/subekti/riyan/firebasesimplecrud/ListActivity.java
+++ app/src/main/java/subekti/riyan/firebasesimplecrud/ListActivity.java
@@ -1,12 +1,16 @@
 package subekti.riyan.firebasesimplecrud;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,6 +32,7 @@ public class ListActivity extends AppCompatActivity {
 
     private RecyclerView rc_list_request;
     private ProgressDialog loading;
+    private FloatingActionButton fab_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,7 @@ public class ListActivity extends AppCompatActivity {
         database = FirebaseDatabase.getInstance().getReference();
 
         rc_list_request = findViewById(R.id.rc_list_request);
+        fab_add = findViewById(R.id.fab_add);
 
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         rc_list_request.setLayoutManager(mLayoutManager);
@@ -66,7 +72,7 @@ public class ListActivity extends AppCompatActivity {
 
 //                Inisialisasi adapter dan data hotel dalam bentuk ArrayList dan mengeset Adapter ke dalam RecyclerView
 
-                requestAdapter = new RequestAdapter(daftarReq);
+                requestAdapter = new RequestAdapter(daftarReq, ListActivity.this);
                 rc_list_request.setAdapter(requestAdapter);
                 loading.dismiss();
             }
@@ -76,6 +82,17 @@ public class ListActivity extends AppCompatActivity {
 //                Kode ini akan dipanggil ketika ada error dan pengambilan data gagal dan memprint errornya ke logcat
                 System.out.println(databaseError.getDetails()+ " " +databaseError.getMessage());
                 loading.dismiss();
+            }
+        });
+
+        findViewById(R.id.fab_add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ListActivity.this, MainActivity.class)
+                        .putExtra("id", "")
+                        .putExtra("nama", "")
+                        .putExtra("email", "")
+                        .putExtra("desk", ""));
             }
         });
     }
--- app/src/main/java/subekti/riyan/firebasesimplecrud/adapter/RequestAdapter.java
+++ app/src/main/java/subekti/riyan/firebasesimplecrud/adapter/RequestAdapter.java
@@ -1,5 +1,8 @@
 package subekti.riyan.firebasesimplecrud.adapter;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.LayoutInflater;
@@ -7,14 +10,17 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import subekti.riyan.firebasesimplecrud.MainActivity;
 import subekti.riyan.firebasesimplecrud.R;
 import subekti.riyan.firebasesimplecrud.model.Requests;
 
 public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.MyViewHolder> {
 
     private List<Requests> moviesList;
+    private Activity mActivity;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public LinearLayout l_layout;
@@ -28,8 +34,9 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.MyViewHo
         }
     }
 
-    public RequestAdapter(List<Requests> moviesList) {
+    public RequestAdapter(List<Requests> moviesList, Activity activity) {
         this.moviesList = moviesList;
+        this.mActivity = activity;
     }
 
     @Override
@@ -46,6 +53,22 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.MyViewHo
 
         holder.tv_nama.setText(movie.getNama());
         holder.tv_email.setText(movie.getEmail());
+
+        holder.l_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent goDetail = new Intent(mActivity, MainActivity.class);
+                goDetail.putExtra("id", movie.getKey());
+                goDetail.putExtra("nama", movie.getNama());
+                goDetail.putExtra("email", movie.getEmail());
+                goDetail.putExtra("desk", movie.getDesk());
+
+                mActivity.startActivity(goDetail);
+
+            }
+
+        });
 
     }
 
